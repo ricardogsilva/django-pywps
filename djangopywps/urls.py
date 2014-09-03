f18393ddd42113b8dada9448bcf16d4dff9d40c2
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2014-09-03 17:17:40 +0100

    worked on the async display of status reports

--- djangopywps/urls.py
+++ djangopywps/urls.py
@@ -4,4 +4,6 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^wpsoutputs/(?P<file_name>pywps-.*?\.xml)$', views.get_status_report,
+        name='get_status_report'),
 )
